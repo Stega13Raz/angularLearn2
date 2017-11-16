0137cd6e69c37234b107319f1b61ab68064ccf4a
Razvan Stegaroiu <razvanstegaroiu@gmail.com>
2017-11-16 16:00:23 +0000

    Update repo

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,17 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app';
+  showDetails = false;
+  logArray = [];
+  arrayCurrIndex = -1;
+
+  onBtnClick() {
+    this.showDetails = !this.showDetails;
+    if (this.showDetails === true) {
+      const today = new Date();
+      const time = today.getHours() + ':' + today.getMinutes() + ':' + today.getSeconds();
+      this.logArray.push(time);
+      this.arrayCurrIndex++;
+    }
+  }
 }
